c634f21e9dbdc5daf9f3ead5b8e143dfaa8ad252
Henry Ije git config --global user.name Henry <henryije@gmail.com>
2023-09-12 21:57:23 +0100

    first commit

--- SignIn/signin.js
+++
@@ -1,9 +0,0 @@
-var email = document.getElementById('email').value;
-var password = document.getElementById('password').value;
-
-localStorage.setItem('email', email);
-localStorage.setItem('password', password);
-
-
-var emailAdd = localStorage.getItem('email');
-var passWord = localStorage.getItem('password');
